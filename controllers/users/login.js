d3d278b5467e2e67e7a96882e9699ae64cb06f54
m3ltman <m3ltman@gmail.com>
2020-03-13 11:47:04 +0500

    Init login controller

---
+++ controllers/users/login.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+const { key } = require('../../configuration/config');
+const UnathorizedError = require('../../errors/Unathorized');
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        key,
+        { expiresIn: '7d' },
+      );
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
+      }).end();
+    })
+    .catch((err) => {
+      if (err.message !== 'Неправильные почта или пароль') {
+        return next(err);
+      }
+      return next(new UnathorizedError(err.message));
+    });
+};
