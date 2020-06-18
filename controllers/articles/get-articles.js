f4762ef85f7bbef9d23ebd4b10855c19e028b4b3
m3ltman <m3ltman@gmail.com>
2020-06-18 21:56:45 +0500

    Add owner object to the article

--- controllers/articles/get-articles.js
+++ controllers/articles/get-articles.js
@@ -3,6 +3,7 @@ const Article = require('../../models/article');
 
 module.exports.getArticles = (req, res, next) => {
   Article.find({})
+    .populate('owner')
     .then((articles) => (articles ? res.status(200).send(articles) : res.status(200).send(articles)))
     .catch(next);
 };
