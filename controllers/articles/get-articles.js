2d6e885251bafd5c713604ec3a8296ce1341572c
m3ltman <m3ltman@gmail.com>
2020-06-20 19:50:44 +0500

    Change getArticles method logic to return only specific user articles

--- controllers/articles/get-articles.js
+++ controllers/articles/get-articles.js
@@ -1,9 +1,12 @@
 /* eslint-disable max-len */
 const Article = require('../../models/article');
+const NotFoundError = require('../../errors/NotFound');
+const { ITEM_NOT_FOUND } = require('../../configuration/constants');
 
 module.exports.getArticles = (req, res, next) => {
-  Article.find({})
+  Article.find({ owner: req.user._id })
+    .orFail(new NotFoundError(ITEM_NOT_FOUND))
     .populate('owner')
-    .then((articles) => (articles ? res.status(200).send(articles) : res.status(200).send(articles)))
+    .then(articles => res.status(200).send(articles))
     .catch(next);
 };
